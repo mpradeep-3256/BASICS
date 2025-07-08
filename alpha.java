4e4516b5458fe9746c866c026bcfd52d5a1ccfbc
mpradeep-3256 <mpradeep0056@gmail.com>
2025-07-08 21:35:10 +0530

    alpha.java

--- alpha.java
+++ alpha.java
@@ -4,7 +4,6 @@ class alpha {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char s = sc.next().charAt(0);
-        // char s2=sc.next().charAt(0);
         if (s >= 'A' && s <= 'z' || s >= 'a' && s <= 'z') {
             System.out.println("Alphabhets");
         } else if (s >= 48 && s <= 57)
